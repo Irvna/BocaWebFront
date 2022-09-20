a4260212e39cc3462d1f0dd204370d97e514b893
Irvna <89928660+Irvna@users.noreply.github.com>
2022-09-20 12:25:52 -0300

    Update index.jsx

--- src/components/Lista/index.jsx
+++ src/components/Lista/index.jsx
@@ -1,6 +1,5 @@
 export const Lista = ({nome, usuario, id, descricao, data_inclusao, palavras_chave}) => {
     return(
-        <div className='lista'>
           <div className="oda">
               <h1>{nome}</h1>
               <p><strong>Usuário:</strong> {usuario}</p>
@@ -8,6 +7,5 @@ export const Lista = ({nome, usuario, id, descricao, data_inclusao, palavras_cha
               <p><strong>Data Inclusão:</strong> {data_inclusao}</p>
               <p><strong>Palavra Chave:</strong> {palavras_chave}</p>
             </div>
-        </div>
     )
 }
